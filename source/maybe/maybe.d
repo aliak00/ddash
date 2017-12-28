9dd4d7f5f263198e98358c7dc72525988829e152
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2017-12-28 18:10:22 +0100

    flatmap + opDispatch

--- source/maybe/maybe.d
+++ source/maybe/maybe.d
@@ -1,6 +1,13 @@
 module maybe;
 
-import std.stdio: writeln;
+version(unittest) {
+    import std.stdio: writeln;
+    import std.array;
+}
+
+template from(string moduleName) {
+    mixin("import from = " ~ moduleName ~ ";");
+}
 
 struct Maybe(T) {
     alias Some = T;
@@ -34,6 +41,19 @@ struct Maybe(T) {
     bool opEquals(Maybe!T rhs) {
         return this.bag == rhs.bag;
     }
+    auto opDispatch(string fn, Args...)(Args args) {
+        alias Fn = () => mixin("front." ~ fn)(args);
+        alias R = typeof(Fn());
+        return empty? maybe!R : maybe(Fn());
+    }
+}
+
+unittest {
+    Maybe!int n = null;
+    assert(n == null);
+    n = 9;
+    assert(n == 9);
+    assert(n != null);
 }
 
 auto maybe(T)(T t) {
@@ -52,41 +72,6 @@ auto isNull(T)(Maybe!T maybe) {
     return maybe == null;
 }
 
-import std.traits;
-import std.range;
-
-template isMaybe(T) {
-    const isMaybe = __traits(compiles, (T.Some a, T.None b) {});
-}
-
-auto fmap(Range)(Range range) if (isInputRange!Range && isMaybe!(ElementType!Range)) {
-    import std.algorithm: filter, map;
-    return range.filter!(a => a != null).map!(a => a.front);
-}
-
-auto fmap(Range)(Range range) if (isInputRange!Range && !isMaybe!(ElementType!Range)) {
-    import std.algorithm: map;
-    return map!(a => a);
-}
-
-unittest {
-    auto arr = [
-        maybe!int,
-        maybe(3), 
-        maybe!int, 
-        maybe(7),
-    ];
-    arr.fmap.writeln;
-}
-
-unittest {
-    Maybe!int n = null;
-    assert(n == null);
-    n = 9;
-    assert(n == 9);
-    assert(n != null);
-}
-
 unittest {
     import std.functional: not;
     import std.algorithm: filter;
@@ -98,4 +83,55 @@ unittest {
         maybe(7),
     ];
     assert(arr.filter!(not!isNull).array == [maybe(3), maybe(7)]);
+}
+
+template isMaybe(T) {
+    static if(is(T U == Maybe!U)) {
+        enum isMaybe = true;
+    } else {
+        enum isMaybe = false;
+    }
+}
+
+unittest {
+    assert(isMaybe!(Maybe!int) == true);
+    assert(isMaybe!int == false);
+    assert(isMaybe!(int[]) == false);
+}
+
+auto flatMap(alias fun, Range)(Range r) if (from!"std.range".isInputRange!Range) {
+    import std.algorithm: map;
+    import std.range: ElementType;
+    static if (isMaybe!(ElementType!Range)) {
+        import std.algorithm: filter;
+        return r.filter!(a => a != null).map!(a => a.front).map!(fun);
+    } else if (__traits(compiles, { ElementType!Range t = null; })) {
+        import std.algorithm: filter;
+        return r.filter!(a => a != null).map!(fun);
+    } else {
+        return r.map!(fun);
+    }
+}
+
+unittest {
+    auto arr = [
+        maybe!int,
+        maybe(3),
+        maybe!int,
+        maybe(7),
+    ];
+    assert(arr.flatMap!(a => a).array == [3, 7]);
+}
+
+unittest {
+    struct Some {
+        int f() {
+            return 7;
+        }
+    }
+    auto a = maybe(Some());
+    auto b = maybe!Some;
+
+    assert(a.f() == maybe(7));
+    assert(b.f() == maybe!int);
 }
