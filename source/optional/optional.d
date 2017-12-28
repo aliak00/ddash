f18da9d06eab39b07e6c629cdc6439677cc59591
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2017-12-29 00:14:12 +0100

    Rename tp optional

--- source/optional/optional.d
+++ source/optional/optional.d
@@ -1,4 +1,4 @@
-module maybe;
+module optional;
 
 version(unittest) {
     import std.stdio: writeln;
@@ -9,11 +9,10 @@ template from(string moduleName) {
     mixin("import from = " ~ moduleName ~ ";");
 }
 
-struct Maybe(T) {
-    alias Some = T;
-    alias None = typeof(null);
+struct Optional(T) {
+    alias Null = typeof(null);
     T[] bag;
-    this(None _) {}
+    this(Null _) {}
     this(T t) {
         this.bag = [t];
     }
@@ -26,49 +25,57 @@ struct Maybe(T) {
     void popFront() {
         this.bag = [];
     }
-    void opAssign(None _) {
+    void opAssign(Null _) {
         this.bag = [];
     }
     void opAssign(T t) {
         this.bag = [t];
     }
-    bool opEquals(None _) {
+    bool opEquals(Null _) {
         return this.bag.length == 0;
     }
     bool opEquals(T rhs) {
         return this.bag.length == 1 && this.bag[0] == rhs;
     }
-    bool opEquals(Maybe!T rhs) {
+    bool opEquals(Optional!T rhs) {
         return this.bag == rhs.bag;
     }
     auto opDispatch(string fn, Args...)(Args args) {
         alias Fn = () => mixin("front." ~ fn)(args);
         alias R = typeof(Fn());
-        return empty? maybe!R : maybe(Fn());
+        return empty? Optional!R() : Optional!R(Fn());
     }
 }
 
 unittest {
-    Maybe!int n = null;
+    Optional!int n = null;
     assert(n == null);
     n = 9;
     assert(n == 9);
     assert(n != null);
 }
 
-auto maybe(T)(T t) {
-    return Maybe!T(t);
+auto optional(T)(T t) {
+    return Optional!T(t);
 }
 
-auto maybe(T)(Maybe!T.None _) {
-    return Maybe!T(_);
+auto optional(T)(Optional!T.Null _) {
+    return Optional!T(_);
 }
 
-auto maybe(T)() {
-    return Maybe!T(null);
+auto optional(T)() {
+    return Optional!T(null);
 }
 
-auto isNull(T)(Maybe!T maybe) {
+auto some(T)(T t) {
+    return Optional!T(t);
+}
+
+auto none(T)() {
+    return Optional!T();
+}
+
+auto isNull(T)(Optional!T maybe) {
     return maybe == null;
 }
 
@@ -77,32 +84,32 @@ unittest {
     import std.algorithm: filter;
     import std.range: array;
     auto arr = [
-        maybe!int,
-        maybe(3), 
-        maybe!int, 
-        maybe(7),
+        optional!int,
+        optional(3), 
+        optional!int, 
+        optional(7),
     ];
-    assert(arr.filter!(not!isNull).array == [maybe(3), maybe(7)]);
+    assert(arr.filter!(not!isNull).array == [some(3), some(7)]);
 }
 
-template isMaybe(T) {
-    static if(is(T U == Maybe!U)) {
-        enum isMaybe = true;
+template isOptional(T) {
+    static if(is(T U == Optional!U)) {
+        enum isOptional = true;
     } else {
-        enum isMaybe = false;
+        enum isOptional = false;
     }
 }
 
 unittest {
-    assert(isMaybe!(Maybe!int) == true);
-    assert(isMaybe!int == false);
-    assert(isMaybe!(int[]) == false);
+    assert(isOptional!(Optional!int) == true);
+    assert(isOptional!int == false);
+    assert(isOptional!(int[]) == false);
 }
 
 auto flatMap(alias fun, Range)(Range r) if (from!"std.range".isInputRange!Range) {
     import std.algorithm: map;
     import std.range: ElementType;
-    static if (isMaybe!(ElementType!Range)) {
+    static if (isOptional!(ElementType!Range)) {
         import std.algorithm: filter;
         return r.filter!(a => a != null).map!(a => a.front).map!(fun);
     } else if (__traits(compiles, { ElementType!Range t = null; })) {
@@ -115,23 +122,23 @@ auto flatMap(alias fun, Range)(Range r) if (from!"std.range".isInputRange!Range)
 
 unittest {
     auto arr = [
-        maybe!int,
-        maybe(3),
-        maybe!int,
-        maybe(7),
+        optional!int,
+        optional(3),
+        optional!int,
+        optional(7),
     ];
     assert(arr.flatMap!(a => a).array == [3, 7]);
 }
 
 unittest {
-    struct Some {
+    struct Object {
         int f() {
             return 7;
         }
     }
-    auto a = maybe(Some());
-    auto b = maybe!Some;
+    auto a = some(Object());
+    auto b = none!Object;
 
-    assert(a.f() == maybe(7));
-    assert(b.f() == maybe!int);
+    assert(a.f() == some(7));
+    assert(b.f() == none!int);
 }
