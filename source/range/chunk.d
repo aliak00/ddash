b77215bf1a441f58f87c179b1dab53772ce000a5
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2017-12-31 12:47:06 +0100

    make chunk(0) work

--- source/range/chunk.d
+++ source/range/chunk.d
@@ -3,12 +3,16 @@ module range.chunk;
 import std.range: isInputRange;
 
 auto chunk(Range)(Range r, size_t size) if (isInputRange!Range) {
-    // TODO: lodash returns a 0 length range if size is zero. Requires a rewrite of the chunks
-    // range.
-    import std.range: chunks;
-    return r.chunks(size);
+    import std.range: chunks, takeNone;
+    if (size) {
+        return r.chunks(size);
+    } else {
+        return r.chunks(1).takeNone;
+    }
 }
 
 unittest {
-    // assert([1, 2, 3].chunk(0).array == (int[]).init);
+    import std.array;
+    assert([1, 2, 3].chunk(0).array == (int[][]).init);
+    assert([1, 2, 3].chunk(1).array == [[1], [2], [3]]);
 }
