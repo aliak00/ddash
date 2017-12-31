f1770dfe9156e7e94f89bd84d501a17ef43e1716
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2017-12-31 14:24:04 +0100

    allow concat of types implicitly castable

--- source/range/concat.d
+++ source/range/concat.d
@@ -7,10 +7,10 @@ auto concat(R, V...)(R range, V values) if (isInputRange!R) {
     static if (V.length) {
         static if (isInputRange!(V[0])) {
             return range.chain(values[0]).concat(values[1..$]);
-        }
-        
-        static if (is(V[0] == ElementType!R)) {
+        } else static if (is(V[0] : ElementType!R)) {
             return range.chain([values[0]]).concat(values[1..$]);
+        } else {
+            static assert(0, "Attempted to concat unsupported type: " ~ V[0].stringof);
         }
     } else {
         return range;
@@ -20,4 +20,7 @@ auto concat(R, V...)(R range, V values) if (isInputRange!R) {
 unittest {
     import std.range: iota, array;
     assert([1, 2, 3].concat(4, [5], [6, 7], 8).array == 1.iota(9).array);
+    assert([1.0].concat(2).array == [1.0, 2.0]);
+    assert([1.0].concat([2, 3]).array == [1.0, 2.0, 3.0]);
+    static assert(!__traits(compiles, [1].concat(2, 3.f)));
 }
