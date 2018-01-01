960a79f61e460efe2d6c85fd928e3ad4bae6b85d
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-01-01 17:12:24 +0100

    countUntil: encapsulate imports

--- source/algorithm/countuntil.d
+++ source/algorithm/countuntil.d
@@ -1,14 +1,15 @@
 module algorithm.countuntil;
 
-import std.algorithm: stdCountUntil = countUntil;
-import optional;
+import optional: no, some;
 
 auto countUntil(alias pred = "a == b", R, Rs...)(R haystack, Rs needles) {
+    import std.algorithm: stdCountUntil = countUntil;
     auto result = stdCountUntil!(pred, R, Rs)(haystack, needles);
     return result == -1 ? no!long : some(result);
 }
 
 unittest {
+    import optional: none;
     assert([1, 2].countUntil(2) == some(1));
     assert([1, 2].countUntil(0) == none);
 }
