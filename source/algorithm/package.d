2bf6755bb3c9eb7a27415e37158c7fbd26f2d394
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-01-03 02:51:50 +0100

    keep track of missing algorithms

--- source/algorithm/package.d
+++ source/algorithm/package.d
@@ -4,7 +4,8 @@ import algorithm.flatmap;
 import algorithm.chunk;
 import algorithm.compact;
 import algorithm.concat;
-import algorithm.difference;
+import algorithm.difference; // also differenceBy
+// TODO: differenceWith
 import std.range: drop;
 import std.range: dropRight = dropBack;
 import algorithm.droprightwhile;
