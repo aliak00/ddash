8b62a02aa260d01e57e841f73bf67ee3ba8c0fbd
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-01-15 01:31:52 +0100

    intersection: make it awesome, also, maybe

--- source/algorithm/intersection.d
+++ source/algorithm/intersection.d
@@ -1,34 +1,62 @@
 module algorithm.intersection;
 
-import std.range: isInputRange;
-import std.stdio;
+import common: from;
 
-struct Intersection(alias pred = "a", R1, R2) if (isInputRange!R1 && isInputRange!R2) {
+struct Intersection(alias pred, R1, R2) if (from!"std.range".isInputRange!R1 && from!"std.range".isInputRange!R2) {
     import std.range: ElementType;
-    import std.functional: unaryFun, binaryFun;
-
-    enum isUnary = is(typeof(unaryFun!pred(ElementType!R1.init)));
-    static if (isUnary)
-    {
-        alias transform = unaryFun!pred;
-        alias compare = binaryFun!"a == b";
-    }
-    else
-    {
-        alias transform = unaryFun!"a";
-        alias compare = binaryFun!pred;
-    }
 
     R1 r1;
     R2 r2;
 
-    alias equal = (a, b) => compare(transform(a), transform(b));
+    alias E = ElementType!R1;
 
     private void moveToNextElement() {
-        import std.algorithm: canFind;
-        import std.range: empty, front, popFront;
-        while (!this.r1.empty && !this.r2.canFind!equal(this.r1.front)) {
-            this.r1.popFront;
+        import utils.traits: isBinaryOver, isSortedRange;
+        static if (!isBinaryOver!(pred, E) && isSortedRange!R1 && isSortedRange!R2)
+        {
+            import std.range: ElementType;
+            import range: sortingPredicate;
+            import utils.traits: isNullType;
+            static if (isNullType!pred)
+            {
+                alias comp = (a, b) => r1.sortingPredicate(a, b);
+            }
+            else
+            {
+                alias comp = (a, b) => r1.sortingPredicate(pred(a), pred(b));
+            }
+
+            while (!this.r1.empty && !this.r2.empty) {
+                if (comp(this.r1.front, this.r2.front)) {
+                    this.r1.popFront;
+                } else if (comp(this.r2.front, this.r1.front)) {
+                    this.r2.popFront;
+                } else {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            import std.algorithm: canFind;
+            import std.range: empty, front, popFront;
+            import utils.traits: isUnaryOver, isNullType;
+            static if (isNullType!pred)
+            {
+                alias equal = (a, b) => a == b;
+            }
+            else static if (isUnaryOver!(pred, E))
+            {
+                alias equal = (a, b) => pred(a) == pred(b);
+            }
+            else
+            {
+                alias equal = (a, b) => pred(a, b);
+            }
+
+            while (!this.r1.empty && !this.r2.canFind!equal(this.r1.front)) {
+                this.r1.popFront;
+            }
         }
     }
 
@@ -40,7 +68,7 @@ struct Intersection(alias pred = "a", R1, R2) if (isInputRange!R1 && isInputRang
 
     bool empty() @property {
         import std.range: empty;
-        return this.r1.empty;
+        return this.r1.empty || this.r2.empty;
     }
     auto front() @property {
         import std.range: front;
@@ -53,18 +81,62 @@ struct Intersection(alias pred = "a", R1, R2) if (isInputRange!R1 && isInputRang
     }
 }
 
-auto intersection(alias pred = "a", Range, Rs...)(Range range, Rs values) if (isInputRange!Range) {
+auto intersection(alias pred = null, Range, Rs...)(Range range, Rs values)
+if (from!"std.range".isInputRange!Range
+    && (from!"utils.traits".isNullType!pred
+        || from!"utils.traits".isUnaryOver!(pred, from!"std.range".ElementType!Range)
+        || from!"utils.traits".isBinaryOver!(pred, from!"std.range".ElementType!Range)))
+{
     static if (!Rs.length)
     {
-        return range;
+        import std.range: takeNone;
+        return range.takeNone;
     }
     else
     {
         import std.range: ElementType;
         import algorithm: concat;
+        import utils.traits: isNullType, isUnaryOver;
+
         auto combinedValues = values.concat;
         static assert (is(ElementType!(typeof(combinedValues)) : ElementType!Range));
-        return Intersection!(pred, typeof(range), typeof(combinedValues))(range, combinedValues);
+
+        // import std.algorithm: sort;
+        // pragma(msg,
+        //     __FUNCTION__,
+        //     "\n  pred: ", typeof(pred),
+        //     "\n  combinedValues: ", typeof(combinedValues),
+        //     "\n  canSortRange: ", is(typeof(range.sort)),
+        //     "\n  canSortCombinedValues: ", is(typeof(combinedValues.sort)),
+        // );
+        static if (isNullType!pred || isUnaryOver!(pred, ElementType!Range))
+        {
+            import std.algorithm: sort;
+            static if (is(typeof(range.sort)))
+            {
+                auto r1 = range.sort;
+            }
+            else
+            {
+                auto r1 = range;
+            }
+
+            static if (is(typeof(combinedValues.sort)))
+            {
+                auto r2 = combinedValues.sort;
+            }
+            else
+            {
+                auto r2 = combinedValues;
+            }
+        }
+        else
+        {
+            auto r1 = range;
+            auto r2 = combinedValues;
+        }
+
+        return Intersection!(pred, typeof(r1), typeof(r2))(r1, r2);
     }
 }
 
