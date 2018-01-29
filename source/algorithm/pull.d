17c5d50df15ae26a4a618d14be97f2be157581ed
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-01-29 21:12:40 +0100

    shorten pull

--- source/algorithm/pull.d
+++ source/algorithm/pull.d
@@ -3,23 +3,9 @@ module algorithm.pull;
 import common;
 
 ref pull(Range, Values...)(return ref Range range, Values values) {
-    import std.algorithm: canFind, moveEmplaceAll;
-    import std.array;
-    import std.range: popBackN, ElementType;
+    import std.algorithm: canFind, remove;
     import algorithm: concat;
-    int numFound = 0;
-    auto needles = concat(values);
-    auto r = range.save;
-    ElementType!Range[] unfoundElements;
-    foreach (e; r) {
-        if (!needles.canFind(e)) {
-            unfoundElements ~= e;
-        } else {
-            numFound++;
-        }
-    }
-    moveEmplaceAll(unfoundElements, range);
-    range.popBackN(numFound);
+    range = range.remove!(a => values.concat.canFind(a));
     return range;
 }
 
