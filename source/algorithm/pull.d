94bab61dcf319bc333b96b74279f650ca309e4ba
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-02-01 23:59:23 +0100

    add pullBy

--- source/algorithm/pull.d
+++ source/algorithm/pull.d
@@ -20,6 +20,17 @@ unittest {
     assert(arr.pull!"a == b"(3, 5) == [1, 2, 4]);
 }
 
+///
+unittest {
+    struct A {
+        int x;
+        int y;
+    }
+
+    auto arr = [A(1, 2), A(3, 4), A(5, 6)];
+    assert(arr.pullBy!"y"(2, 6) == [A(3, 4)]);
+}
+
 import common;
 
 /**
@@ -28,16 +39,49 @@ import common;
     Params:
         range = a mutable range
         values = variables args of ranges and values to pull out
+        pred = a unary transoform predicate or binary equality predicate. Defaults to `==`.
 
     Returns:
         Modified range
 */
-ref pull(alias pred = null, Range, Values...)(return ref Range range, Values values) {
+ref pull(alias pred = null, Range, Values...)(return ref Range range, Values values)
+if (from!"std.range".isInputRange!Range)
+{
+    return range.pullBase!("", pred)(values);
+}
+
+/**
+    Removes elements from a range by a publicly visible member variable or property of `ElemntType!Range`
+
+    Params:
+        range = a mutable range
+        values = variables args of ranges and values to pull out
+        member = which member in `ElementType!Range` to pull by
+        pred = a unary transform predicate or binary equality predicate. Defaults to `==`.
+
+    Returns:
+        Modified range
+*/
+ref pullBy(string member, alias pred = null, Range, Values...)(return ref Range range, Values values)
+if (from!"std.range".isInputRange!Range && member.length)
+{
+    return range.pullBase!(member, pred)(values);
+}
+
+ref pullBase(string member, alias pred, Range, Values...)(return ref Range range, Values values) {
     import std.algorithm: canFind, remove;
     import algorithm: concat;
-    import internal: equalityComparator;
+    import internal: equalityComparator, valueBy;
     alias equal = (a, b) => equalityComparator!pred(a, b);
     auto unwanted = concat(values);
-    range = range.remove!(a => unwanted.canFind!equal(a));
+    static if (member == "")
+    {
+        alias f = (a) => a;
+    }
+    else
+    {
+        alias f = (a) => a.valueBy!member;
+    }
+    range = range.remove!(a => unwanted.canFind!equal(f(a)));
     return range;
 }
