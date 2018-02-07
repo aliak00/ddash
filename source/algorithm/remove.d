11d8579db855d96aaca7fb9d26303bcd45e152b8
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-02-07 19:48:04 +0100

    remove: mutating algo probably should return void

--- source/algorithm/remove.d
+++ source/algorithm/remove.d
@@ -3,10 +3,8 @@ module algorithm.remove;
 ///
 unittest {
     auto arr = [1, 2, 3, 4];
-    auto odds = arr.remove!(a => a % 2 == 0);
+    arr.remove!(a => a % 2 == 0);
     assert(arr.equal([1, 3]));
-    assert(odds.equal([1, 3]));
-    assert(odds.equal(arr));
 }
 
 import common;
@@ -16,12 +14,8 @@ import common;
 
     Params:
         pred = unary predicate that returns true if you want an element removed
-
-    Returns:
-        The range you passed in, with elements removed
 */
-ref remove(alias pred, Range)(ref Range range) if (from!"std.range".isInputRange!Range) {
+void remove(alias pred, Range)(ref Range range) if (from!"std.range".isInputRange!Range) {
     import std.algorithm: stdRemove = remove;
     range = range.stdRemove!pred;
-    return range;
 }
