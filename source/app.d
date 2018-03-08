0be8c411189cfebb7b6170ff8fbc4f1e1e5285cb
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-03-08 20:46:40 +0100

    Add imports to app

--- source/app.d
+++ source/app.d
@@ -1,5 +1,5 @@
 import std.stdio;
-import std.functional, std.algorithm, std.range, std.array;
+import std.functional, std.algorithm, std.range, std.array, std.traits, std.meta;
 import common, algorithm;
 
 void main() {
