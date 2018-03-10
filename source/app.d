3a16075b1a6ec540c45b56480c87c4f0cadd3e45
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-03-11 00:12:15 +0100

    import functional in main

--- source/app.d
+++ source/app.d
@@ -1,5 +1,5 @@
 import std.stdio;
-import std.functional, std.algorithm, std.range, std.array, std.traits, std.meta;
+import std.functional, std.algorithm, std.range, std.array, std.traits, std.meta, std.functional;
 import common, algorithm;
 
 void main() {
