34c912fdebb018e69d8bead16c8097316c0e4471
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-03-12 00:00:58 +0100

    import typecons in app.d

--- source/app.d
+++ source/app.d
@@ -1,5 +1,5 @@
 import std.stdio;
-import std.functional, std.algorithm, std.range, std.array, std.traits, std.meta, std.functional;
+import std.functional, std.algorithm, std.range, std.array, std.traits, std.meta, std.functional, std.typecons;
 import common, algorithm;
 
 void main() {
