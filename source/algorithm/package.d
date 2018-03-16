7c05ed792bec562120ee08ea40359337669ee73f
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-03-16 21:18:48 +0100

    docs

--- source/algorithm/package.d
+++ source/algorithm/package.d
@@ -2,10 +2,11 @@
     Contains a number of algorithms
 
 $(TABLE
-$(TR $(TH Module) $(TH Function Name) $(TH Description))
+$(TR $(TH Module) $(TH Functions) $(TH Properties) $(TH Description))
 $(TR
     $(TD `algorithm.chunk`)
     $(TD $(DDOX_NAMED_REF algorithm.chunk, `chunk`))
+    $(TD)
     $(TD Creates an array of elements split into groups the length of size.)
     )
 $(TR
@@ -15,11 +16,13 @@ $(TR
         $(DDOX_NAMED_REF algorithm.compact.compactBy, `compactBy`)<br>
         $(DDOX_NAMED_REF algorithm.compact.compactValues, `compactValues`)<br>
         )
+    $(TD)
     $(TD Creates a range with all falsey values removed.)
     )
 $(TR
     $(TD `algorithm.concat`)
     $(TD $(DDOX_NAMED_REF algorithm.concat, `concat`))
+    $(TD)
     $(TD Concatenate ranges and values together to a new range)
     )
 $(TR
@@ -28,6 +31,7 @@ $(TR
         $(DDOX_NAMED_REF algorithm.difference.difference, `difference`)<br>
         $(DDOX_NAMED_REF algorithm.difference.differenceBy, `differenceBy`)<br>
         )
+    $(TD)
     $(TD Creates a range of values not included in the other given set of values)
     )
 $(TR
@@ -38,6 +42,7 @@ $(TR
         $(DDOX_NAMED_REF algorithm.drop.dropWhile, `dropWhile`)<br>
         $(DDOX_NAMED_REF algorithm.drop.dropRightWhile, `dropRightWhile`)<br>
         )
+    $(TD)
     $(TD Drops elements from a range)
     )
 $(TR
@@ -46,16 +51,19 @@ $(TR
         $(DDOX_NAMED_REF algorithm.equal.equal, `equal`)<br>
         $(DDOX_NAMED_REF algorithm.equal.equalBy, `equalBy`)<br>
         )
+    $(TD)
     $(TD Tells you if two things are equal)
     )
 $(TR
     $(TD `algorithm.fill`)
     $(TD $(DDOX_NAMED_REF algorithm.fill, `fill`))
+    $(TD mutates)
     $(TD Assigns value to each element of input range.)
     )
 $(TR
     $(TD `algorithm.first`)
     $(TD `first`)
+    $(TD)
     $(TD Returns `optional` front of range)
     )
 $(TR
@@ -64,16 +72,19 @@ $(TR
         $(DDOX_NAMED_REF algorithm.flatten.flatten, `flatten`)<br>
         $(DDOX_NAMED_REF algorithm.flatten.flattenDeep, `flattenDeep`)<br>
         )
+    $(TD)
     $(TD Flattens a range by removing $(DDOX_NAMED_REF utils.isFalsey, `falsey`) values)
     )
 $(TR
     $(TD `algorithm.frompairs`)
     $(TD $(DDOX_NAMED_REF algorithm.frompairs, `fromPairs`))
+    $(TD)
     $(TD Returns a newly allocated associative array from a range of key/value tuples)
     )
 $(TR
     $(TD -)
     $(TD `last`)
+    $(TD)
     $(TD Returns `optional` back of range)
     )
 $(TR
@@ -84,26 +95,31 @@ $(TR
         $(DDOX_NAMED_REF algorithm.index.indexOf, `indexOf`)<br>
         $(DDOX_NAMED_REF algorithm.index.lastIndexOf, `lastIndexOf`)<br>
         )
+    $(TD)
     $(TD Returns `optional` index of an element in a range.)
     )
 $(TR
     $(TD -)
     $(TD `initial`)
+    $(TD)
     $(TD Gets all but the last element of a range)
     )
 $(TR
     $(TD `algorithm.intersection`)
     $(TD $(DDOX_NAMED_REF algorithm.intersection, `intersection`))
+    $(TD)
     $(TD Creates a range of unique values that are included in the other given set of values)
     )
 $(TR
     $(TD `algorithm.join`)
     $(TD $(DDOX_NAMED_REF algorithm.join, `join`))
+    $(TD)
     $(TD Converts all elements in range into a string separated by separator.)
     )
 $(TR
     $(TD `algorithm.nth`)
     $(TD $(DDOX_NAMED_REF algorithm.nth, `nth`))
+    $(TD)
     $(TD Returns the element at nth index of range)
     )
 $(TR
@@ -113,31 +129,37 @@ $(TR
         $(DDOX_NAMED_REF algorithm.pull.pullAt, `pullAt`)<br>
         $(DDOX_NAMED_REF algorithm.pull.pullBy, `pullBy`)<br>
         )
+    $(TD)
     $(TD Pulls elements out of a range)
     )
 $(TR
     $(TD `algorithm.remove`)
     $(TD $(DDOX_NAMED_REF algorithm.remove, `remove`))
+    $(TD mutates)
     $(TD Removed elements from a range by unary predicate)
     )
 $(TR
-    $(TD -)
+    $(TD)
     $(TD `reverse`)
+    $(TD mutates)
     $(TD Reverses a range in place)
     )
 $(TR
     $(TD `algorithm.slice`)
     $(TD $(DDOX_NAMED_REF algorithm.slice, `slice`))
+    $(TD)
     $(TD Creates a slice of range from start up to, but not including, end)
     )
 $(TR
     $(TD `algorithm.sort`)
     $(TD $(DDOX_NAMED_REF algorithm.sort.sortBy, `sortBy`))
+    $(TD)
     $(TD Provides various ways for sorting a range)
     )
 $(TR
     $(TD `algorithm.zip`)
     $(TD $(DDOX_NAMED_REF algorithm.zip.zipEach, `zipEach`))
+    $(TD)
     $(TD Zips up ranges together)
     )
 )
