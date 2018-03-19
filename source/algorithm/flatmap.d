0d2f0d8c5a31ae1af749df68b8d412a2faafba78
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-03-19 22:19:58 +0100

    flatmap should map and then flatten

--- source/algorithm/flatmap.d
+++ source/algorithm/flatmap.d
@@ -1,13 +1,35 @@
 module algorithm.flatmap;
-
 import common;
 
+///
+unittest {
+    auto dup(int n) {
+        return [n, n];
+    }
+
+    assert([1, 2].flatMap!dup.equal([1, 1, 2, 2]));
+
+    import optional: some, no;
+
+    assert([
+        no!int,
+        some(3),
+        no!int,
+        some(7),
+    ].flatMap!"a".equal(
+        [3, 7]
+    ));
+}
+
+/**
+    Flatmaps a range of elemenents
+*/
 auto flatMap(alias fun, Range)(Range range) if (from!"std.range".isInputRange!Range) {
     import std.algorithm: map;
     import algorithm: flatten;
     return range
-        .flatten
-        .map!(fun);
+        .map!fun
+        .flatten;
 }
 
 unittest {
@@ -43,8 +65,8 @@ unittest {
     auto intArrayOfArrays = [
         [3],
         [],
-        [7],
+        [7, 8],
         [],
     ];
-    assert(intArrayOfArrays.flatMap!(a => a).equal([3, 7]));
+    assert(intArrayOfArrays.flatMap!(a => a).equal([3, 7, 8]));
 }
