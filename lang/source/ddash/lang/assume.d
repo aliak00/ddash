a8befce6ae84419b7c5de7132308ab1409dbffd7
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-09-04 00:23:16 +0200

    add assume pure

--- lang/source/ddash/lang/assume.d
+++ lang/source/ddash/lang/assume.d
@@ -10,32 +10,53 @@ template assume(alias fun) {
         enum attrs = functionAttributes!T | assumedAttr;
         return cast(SetFunctionAttributes!(T, functionLinkage!T, attrs)) t;
     }
-    auto ref nogc_(Args...)(auto ref Args args) {
-        enum call = "assumeAttribute!(FunctionAttribute.nogc)((ref Args args) { fun(args); })(args)";
-        static assert(
-            __traits(compiles, {
+    private static impl(string attr) {
+        return `
+            enum call = "assumeAttribute!(`~attr~`)((ref Args args) { return fun(args); })(args)";
+            ` ~ q{
+            static assert(
+                __traits(compiles, {
+                    mixin(call ~ ";");
+                }),
+                "function " ~ fun.stringof ~ " is not callable with args " ~ Args.stringof
+            );
+            alias R = typeof(mixin(call));
+            static if (is(R == void)) {
                 mixin(call ~ ";");
-            }),
-            "function " ~ fun.stringof ~ " is not callable with args " ~ Args.stringof
-        );
-        alias R = typeof(mixin(call));
-        static if (is(R == void)) {
-            mixin(call ~ ";");
-        } else {
-            return mixin(call ~ ";");
-        }
+            } else {
+                mixin("return " ~ call ~ ";");
+            }
+        };
+    }
+    auto ref nogc_(Args...)(auto ref Args args) {
+        mixin(impl("FunctionAttribute.nogc"));
+    }
+    auto ref pure_(Args...)(auto ref Args args) {
+        mixin(impl("FunctionAttribute.pure_"));
     }
 }
 
 ///
 @nogc unittest {
+    static b = [1];
     auto allocates() {
         return [1];
     }
-    assume!allocates.nogc_();
+    auto a = assume!allocates.nogc_();
+    assert(a == b);
 
     auto something(int a) {
         allocates;
     }
     assume!something.nogc_(3);
+}
+
+///
+unittest {
+    static int thing = 0;
+    alias lambda = () => thing++;
+    () pure {
+        cast(void)assume!lambda.pure_();
+    }();
+    assert(thing == 1);
 }
